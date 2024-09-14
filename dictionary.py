19ccc6f481e71451d61d9e1ac053a7e1539443b8
beastamrit <mainamritjha@gmail.com>
2024-09-14 20:43:47 +0530

    dictionary in python

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+a={"Name":"Messi","Age":36,"Gender":"Male"}
+print(a["Name"])
+a["Age"]=38
+print(a)
+b={"codingal":2,"is":2,"for":2,"coding":1}
+print (str(b))
+k=2
+result=0
+for i in b :
+    if b[i]==2:
+        result=result+1
+print(str(result))
